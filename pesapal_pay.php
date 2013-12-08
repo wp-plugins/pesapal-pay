91bed16166ce95cac340f1208e51a893fa8b6aa9
rixeo <rixeo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-08 11:36:31 +0000

    Author info
    git-svn-id: https://plugins.svn.wordpress.org/pesapal-pay/trunk@816826 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pesapal_pay.php
+++ pesapal_pay.php
@@ -3,7 +3,7 @@
 Plugin Name: Pesapal Pay
 Description: A quick way to integrate pesapal to your website to handle the payment process. All you need to do is set up what parameters to capture from the form and the plugin will do the rest
 Version: 1.2
-Author: rixeo
+Author: rixed
 Author URI: http://thebunch.co.ke/
 Plugin URI: http://thebunch.co.ke/
 */
